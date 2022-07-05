1a54e66ad6071f6ab2a6054c1a33fa9e68b75bce
Emir <emirsoyalan79@gmail.com>
2022-07-06 01:51:39 +0300

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { 
+    Button,
+    Grid,
+    IconButton,
+    Typography
+}from '@mui/material';
+import LanguageIcon from '@mui/icons-material/Language';
 import MediumCarousel from './Components/MediumCarousel';
 import Cookies from 'js-cookie';
+import {texts, getText} from './globals.js';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lang: props.lang
+        }
+        this.swapLangHandler = props.swapLangHandler;
+    }
+    componentWillReceiveProps(props){
+        this.setState({lang: props.lang})
+    }
     render(){
         return  (
-            <div>
-                <Navbar></Navbar>
-            </div>
+            <Navbar lang={this.state.lang} swapLangHandler={this.swapLangHandler}></Navbar>
         )
     }
 }
+
+function MenuButton(props){
+    var style = {color:'white', fontSize: 25}
+    var button = (
+        <Button>
+            <Typography style={style} variant="body2" color="text.secondary">
+                {props.children}
+            </Typography>
+        </Button>
+    )
+    if (props.onClick){
+        button = (
+            <Button
+            onClick={()=>{props.onClick()}}
+            >
+                <Typography style={style} variant="body2" color="text.secondary">
+                    {props.children}
+                </Typography>
+            </Button>
+        )
+    }
+    return button
+    
+}
+
 class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lang: props.lang
+        }
+        this.swapLangHandler = props.swapLangHandler;
+    }
+    componentWillReceiveProps(props){
+        this.setState({lang: props.lang})
+    }
+
     render(){
         return (
-            <div>
-
-            </div>
-        )
+            <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            style={{height:100, background: "linear-gradient(90deg, rgba(2,0,36,1) 11%, rgba(32,54,128,1) 68%, rgba(130,156,208,1) 100%)"}}
+            >
+                <Grid 
+                container
+                position="fixed"
+                xs={7}
+                justifyContent="space-between"
+                style = {{minWidth: 700, paddingInline:30, background:''}}
+                >
+                    <MenuButton onClick={()=>{window.location.reload(false);}}>
+                        {getText(texts.header.navbar.profile, this.state.lang)}
+                    </MenuButton>
+                    <MenuButton>
+                        {getText(texts.header.navbar.projects, this.state.lang)}
+                    </MenuButton>
+                    <MenuButton>
+                        {getText(texts.header.navbar.blog, this.state.lang)}
+                    </MenuButton>
+                    <MenuButton>
+                        {getText(texts.header.navbar.contact, this.state.lang)}
+                    </MenuButton>
+                    <MenuButton>
+                        {getText(texts.header.navbar.support, this.state.lang)}
+                    </MenuButton>
+                </Grid>
+                <Grid container xs></Grid>
+                <Grid 
+                container
+                xs={1}
+                justifyContent="space-between"
+                style = {{paddingInline:40, color:'white', fontSize: 26, background:''}}
+                >
+                    <IconButton
+                    size="large"
+                    color="inherit"
+                    onClick={()=>{this.swapLangHandler()}}
+                    >
+                        <LanguageIcon style={{fontSize:30}} />
+                    </IconButton>
+                    </Grid>
+            </Grid>
+            );
+        }
     }
-}
 
 class Body extends React.Component {
     render(){
@@ -76,14 +170,15 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            lang: (Cookies.get('lang') || 'en')
+            lang: (Cookies.get('lang') || 'en'),
         }
+        this.swapLangHandler = this.handleSwapLang.bind(this)
         console.log('construct', this.state.lang, Cookies.get('lang'));
     }
 
     // right now only supports Turkish and English, so it's only a binary swap
     // TODO: the function has to be rewritten should more languages are supported  
-    swapLang(){
+    handleSwapLang(){
         var newLang = this.state.lang === 'tr' ? 'en' : 'tr'
         this.setState({
             lang: newLang
@@ -94,7 +189,7 @@ class App extends React.Component {
     render(){
         return (
             <>
-                <Header></Header>
+                <Header lang={this.state.lang} swapLangHandler={this.swapLangHandler}></Header>
                 <Body></Body>
                 <MediumCarousel lang={this.state.lang}></MediumCarousel>
                 <Footer></Footer>
