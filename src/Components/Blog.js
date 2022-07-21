b421eda728c93cdeb3a7d8dc9f4b7541b590f3f5
Emir <emirsoyalan79@gmail.com>
2022-07-21 06:00:50 +0300

    Add blog component

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
 import MediumCarousel from './MediumCarousel';
+import { texts, getText } from '../globals';
 
 class Blog extends React.Component {
     render(){
         return (
-            <MediumCarousel lang={this.props.lang}></MediumCarousel>
+            <Stack>
+                <Typography style={{margin:"auto", paddingY: 50, filter: "drop-shadow(0px 0px 10px rgba(255, 255, 255, .6))"}} variant="h2" color="white">
+                    {getText(texts.body.blog.title, this.props.lang)}
+                </Typography>
+                <MediumCarousel lang={this.props.lang}></MediumCarousel>
+            </Stack>
         )
     }
 }
