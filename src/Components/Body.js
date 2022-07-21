414784e31c658548a7c9960237083898308386e1
Emir <emirsoyalan79@gmail.com>
2022-07-21 05:59:03 +0300

    Adjust body spacing

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -14,7 +14,7 @@ class Body extends React.Component {
         return (
             <Container maxWidth="0" disableGutters>
                 <Box display="flex" justifyContent="center" style={{background: "linear-gradient(90deg, rgba(9,9,9,1) 0%, rgba(41,41,41,1) 72%, rgba(102,102,102,1) 98%, rgba(111,111,111,1) 100%)", boxShadow: "0px -30px 30px 8px rgba(0,0,0,0.9) inset"}} >
-                    <Stack width="100%" spacing={15} divider={<Divider orientation="horizontal" flexItem />}>
+                    <Stack width="100%" spacing={20} divider={<Divider orientation="horizontal" flexItem />}>
                         <Profile lang={this.props.lang}></Profile>
                         <Projects lang={this.props.lang}></Projects>
                         <Blog lang={this.props.lang}></Blog>
