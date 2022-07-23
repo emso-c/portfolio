ee9b124bdfb8ba3330ca344e673139f4af21871a
Emir <emirsoyalan79@gmail.com>
2022-07-23 12:37:33 +0300

    Change body bgcolor

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -13,7 +13,7 @@ class Body extends React.Component {
     render(){
         return (
             <Container maxWidth="0" disableGutters>
-                <Box display="flex" justifyContent="center" style={{background: "linear-gradient(90deg, rgba(9,9,9,1) 0%, rgba(41,41,41,1) 72%, rgba(102,102,102,1) 98%, rgba(111,111,111,1) 100%)", boxShadow: "0px -30px 30px 8px rgba(0,0,0,0.9) inset"}} >
+                <Box display="flex" justifyContent="center" style={{background: "linear-gradient(90deg, rgba(9,9,9,1) 0%, rgba(41,41,41,1) 81%, rgba(51,51,51,1) 92%, rgba(62,62,62,1) 100%)", boxShadow: "0px -30px 30px 8px rgba(0,0,0,0.9) inset"}} >
                     <Stack width="100%" spacing={20} divider={<Divider orientation="horizontal" flexItem />}>
                         <Profile lang={this.props.lang}></Profile>
                         <Projects lang={this.props.lang}></Projects>
