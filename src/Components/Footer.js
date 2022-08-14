7f904c2acd6f80a7e5c49cf527aa2d1a537ee0e4
Emir <emirsoyalan79@gmail.com>
2022-08-14 18:22:43 +0300

    Add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Typography } from '@mui/material';
 
 class Footer extends React.Component {
     render(){
         return (
-            <>
-            </>
+            <Typography style={{background: 'black', color: 'white', margin: 0, paddingBottom: 10, display:'flex', justifyContent: 'center'}}>
+                <small>Copyright &copy; Emir Soyalan 2022 | All rights reserved.</small>
+            </Typography>
         )
     }
 }
