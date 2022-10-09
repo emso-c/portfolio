c65b10adff31929739df43ab04694fce7031580a
Emir <emirsoyalan79@gmail.com>
2022-10-09 17:32:07 +0300

    Add animation on scroll

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -10,6 +10,8 @@ import {
     Stack,
     Divider
 } from '@mui/material';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import "animate.css/animate.min.css";
 
 class Body extends React.Component {
     render(){
@@ -18,9 +20,15 @@ class Body extends React.Component {
                 <Box display="flex" justifyContent="center" style={{background: "linear-gradient(90deg, rgba(9,9,9,1) 0%, rgba(41,41,41,1) 81%, rgba(51,51,51,1) 92%, rgba(62,62,62,1) 100%)", boxShadow: "0px -30px 30px 8px rgba(0,0,0,0.9) inset"}} >
                     <Stack width="100%" spacing={20} divider={<Divider orientation="horizontal" flexItem />}>
                         <Profile lang={this.props.lang}></Profile>
-                        <Projects lang={this.props.lang}></Projects>
-                        <Blog lang={this.props.lang}></Blog>
-                        <Contact lang={this.props.lang}></Contact>
+                        <AnimationOnScroll initiallyVisible={false} animateIn="animate__fadeIn" duration={2} animateOnce>
+                            <Projects lang={this.props.lang}></Projects>
+                        </AnimationOnScroll>
+                        <AnimationOnScroll initiallyVisible={false} animateIn="animate__fadeIn" duration={2} animateOnce>
+                            <Blog lang={this.props.lang}></Blog>
+                        </AnimationOnScroll>
+                        <AnimationOnScroll initiallyVisible={false} animateIn="animate__fadeIn" duration={2} animateOnce>
+                            <Contact lang={this.props.lang}></Contact>
+                        </AnimationOnScroll>
                         {/* TODO <Donate lang={this.props.lang}></Donate> */}
                         <br></br>
                         <br></br>
